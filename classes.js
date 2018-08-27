19863b1d96e5c96a79fc7c2ec0cc549c4096e003
Your Name <you@example.com>
2018-08-27 18:30:03 +1200

    makeVisible working

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -32,3 +33,9 @@ function three () {
   one.classList.add('yellow')
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.remove('invisible')[0]
+  makeVisible.classList.add('visible')[0]
+}
+
